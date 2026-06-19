b6131e3f4d949a92ba721fa787b0c30a7ae2e744
Julio Robles <julioroblesb13@gmail.com>
2026-06-19 17:51:59 -0500

    fix: import useEffect in CampaignSender

--- apps/web/src/modules/campaigns/components/CampaignSender.tsx
+++ apps/web/src/modules/campaigns/components/CampaignSender.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { useMarketingContacts } from '../../../hooks/queries/useMarketingContacts';
 import { useCreateCampaign } from '../../../hooks/queries/useCampaigns';
 import { useCampaignMediaUpload } from '../../../hooks/media/useCampaignMediaUpload';
